25ffecfa8b557e728fa2d611be4230a6bc5120fa
Ítalo Guilherme <italoguilherme.glhrm@gmail.com>
2025-05-01 19:44:14 -0300

    fix: opcao 2

--- t3.py
+++ t3.py
@@ -2,13 +2,13 @@ import pyodbc
 
 connection_string = (
     r"DRIVER={ODBC Driver 17 for SQL Server};"
-    r"SERVER=PMW-L05N41H611\SQLEXPRESS;"
+    r"SERVER=localhost;"
     r"DATABASE=ATV3;"
     r"UID=sa;"
     r"PWD=1234"
 )
-conn = pyodbc.connect(connection_string)
 
+conn = pyodbc.connect(connection_string)
 cursor = conn.cursor()
 
 menu = '1 - Consultar saldo \n' \
@@ -20,25 +20,29 @@ escolha = int(input())
 if escolha == 1:
     idOrigem = int(input("Digite o ID da conta: "))
     query = 'SELECT saldo FROM Contas WHERE idConta = ?'
-    cursor.execute(query, (idOrigem, ))
-    results = cursor.fetchone()
-    if results:
-        saldo = results[0]
-        print (f'Saldo da conta {idOrigem}: R${float(saldo)}')
+    cursor.execute(query, (idOrigem,))
+    result = cursor.fetchone()
+    if result:
+        saldo = result[0]
+        print(f'Saldo da conta {idOrigem}: R${float(saldo):.2f}')
     else:
         print("Conta não encontrada!")
-    
+        
 
 elif escolha == 2:
-    mensagem = ''
     idOrigem = int(input("Digite o ID da conta de origem: "))
     idDestino = int(input("Digite o ID da conta de destino: "))
     valor = float(input("Digite o valor da transferência: "))
-    query = "SET NOCOUNT ON ; EXEC sp_RealizarTransferencia ?, ?, ?, ?"
-    cursor.execute(query, (idOrigem, idDestino, valor, mensagem))
-    conn.commit()
-    mensagem = cursor.fetchone()
+
+    query = """
+    DECLARE @mensagem VARCHAR(100);
+    EXEC sp_RealizarTransferencia ?, ?, ?, @mensagem OUTPUT;
+    SELECT @mensagem AS msg;
+    """
+    cursor.execute(query, (idOrigem, idDestino, valor))
+    mensagem = cursor.fetchone()[0]
     print(mensagem)
+    conn.commit()
 
 else:
     print("Escolha inválida")
